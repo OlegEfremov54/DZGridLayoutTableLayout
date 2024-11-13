bda2ef53e3f9ad4a3c132f6ae9b333a61d5c5858
OlegEfremov54 <ole_f@mail.ru>
2024-11-13 17:15:55 +0300

    комитФинал

--- app/src/main/java/com/example/dzgridlayouttablelayout/CountViewModel.kt
+++ app/src/main/java/com/example/dzgridlayouttablelayout/CountViewModel.kt
@@ -4,8 +4,71 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CountViewModel: ViewModel() {
-    lateinit var inputTV:String
-    lateinit var resultTV:String
-    val corentActiv:MutableLiveData <String> by lazy { MutableLiveData<String>() }
 
+    val result:MutableLiveData <String> by lazy { MutableLiveData<String>() }
+
+    fun result(stroka: String) {
+        var prev: String = ""
+        var next: String = ""
+        val size = stroka.length
+        var operand: String = ""
+
+        val lastElement: Char = stroka[stroka.length - 1]
+
+        if (lastElement == '+' ||
+            lastElement == '-' ||
+            lastElement == '*' ||
+            lastElement == '/'
+        ) {
+            result.value = "Не хватает данных"
+        }
+
+        else if (stroka.length > 2) {
+            for (i in stroka.indices) {
+                if (stroka[i] == '+' ||
+                    stroka[i] == '-' ||
+                    stroka[i] == '/' ||
+                    stroka[i] == '*'
+                ) {
+                    operand = stroka[i].toString()
+                    for (i in 0..i - 1) {
+                        prev += stroka[i]
+                    }
+                    for (i in i + 1..size - 1) {
+                        next += stroka[i]
+                    }
+                }
+            }
+        }
+
+        if (prev != "" && next != "" && operand != "") {
+            when (operand) {
+                "+" -> {
+                    val res = prev.toDouble() + next.toDouble()
+                    result.value = res.toString()
+                }
+
+                "-" -> {
+                    val res = prev.toDouble() - next.toDouble()
+                    result.value = res.toString()
+                }
+
+                "*" -> {
+                    val res = prev.toDouble() * next.toDouble()
+                    result.value = res.toString()
+                }
+
+                "/" -> {
+                    if (prev != "0" && next != "0") {
+                        val res = prev.toDouble() / next.toDouble()
+                        result.value = res.toString()
+                    } else {
+                        result.value = "Делить на 0 нельзя!"
+                    }
+                }
+            }
+        } else {
+            result.value = "Данные не введены"
+        }
+    }
 }
--- app/src/main/java/com/example/dzgridlayouttablelayout/MainActivity.kt
+++ app/src/main/java/com/example/dzgridlayouttablelayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dzgridlayouttablelayout
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -16,6 +17,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     private lateinit var toolbarMain: Toolbar
 
     lateinit var countViewModel: CountViewModel
@@ -40,7 +42,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var resultBTN: Button
     private lateinit var resetBTN: Button
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -51,14 +52,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             insets
         }
 
+        countViewModel = ViewModelProvider(this)[CountViewModel::class.java]
 
-    countViewModel=ViewModelProvider(this)[CountViewModel::class.java]
-
-        countViewModel.corentActiv.observe(this, Observer {
-        inputTV.text=it.toString()
+        countViewModel.result.observe(this, Observer {
+            resultTV.text = it
         })
-
-
 
         toolbarMain = findViewById(R.id.toolbarMain)
         setSupportActionBar(toolbarMain)
@@ -69,6 +67,80 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         inputTV = findViewById(R.id.inputTV)
         resultTV = findViewById(R.id.resultTV)
 
+        extracted()
+        extractedClick()
+    }
+
+    override fun onClick(v: View) {
+        if (inputTV.text.isEmpty()) { return }
+        when (v.id) {
+            R.id.zeroBTN -> addValueInInputTV("0")
+            R.id.oneBTN -> addValueInInputTV("1")
+            R.id.twoBTN -> addValueInInputTV("2")
+            R.id.threeBTN -> addValueInInputTV("3")
+            R.id.fourBTN -> addValueInInputTV("4")
+            R.id.fiveBTN -> addValueInInputTV("5")
+            R.id.sixBTN -> addValueInInputTV("6")
+            R.id.sevenBTN -> addValueInInputTV("7")
+            R.id.eightBTN -> addValueInInputTV("8")
+            R.id.nineBTN -> addValueInInputTV("9")
+            R.id.sumBTN -> addOperandInInputTV("+")
+            R.id.difBTN -> addOperandInInputTV("-")
+            R.id.multBTN -> addOperandInInputTV("*")
+            R.id.divBTN -> addOperandInInputTV("/")
+            R.id.resultBTN -> countViewModel.result(inputTV.text.toString())
+            R.id.resetBTN -> {
+                inputTV.text = "O"
+                resultTV.text = "Результат"
+            }
+            else -> {
+                inputTV.text = "O"
+                resultTV.text = "Результат"
+            }
+        }
+    }
+
+    fun addValueInInputTV(str: String) {
+        if (inputTV.text.toString() == "O") {
+            inputTV.text = str
+        } else {
+            inputTV.text = (buildString {
+                append(inputTV.text.toString())
+                append(str)
+            })
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState?.run{putString("key", inputTV.text.toString())}
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputTV.text=savedInstanceState?.getString("key")
+    }
+
+    private fun extractedClick() {
+        zeroBTN.setOnClickListener(this)
+        oneBTN.setOnClickListener(this)
+        twoBTN.setOnClickListener(this)
+        threeBTN.setOnClickListener(this)
+        fourBTN.setOnClickListener(this)
+        fiveBTN.setOnClickListener(this)
+        sixBTN.setOnClickListener(this)
+        sevenBTN.setOnClickListener(this)
+        eightBTN.setOnClickListener(this)
+        nineBTN.setOnClickListener(this)
+        sumBTN.setOnClickListener(this)
+        divBTN.setOnClickListener(this)
+        multBTN.setOnClickListener(this)
+        difBTN.setOnClickListener(this)
+        resultBTN.setOnClickListener(this)
+        resetBTN.setOnClickListener(this)
+    }
+
+    private fun extracted() {
         zeroBTN = findViewById(R.id.zeroBTN)
         oneBTN = findViewById(R.id.oneBTN)
         twoBTN = findViewById(R.id.twoBTN)
@@ -87,174 +159,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         resultBTN = findViewById(R.id.resultBTN)
         resetBTN = findViewById(R.id.resetBTN)
-
-
-        zeroBTN.setOnClickListener(this)
-        oneBTN.setOnClickListener(this)
-        twoBTN.setOnClickListener(this)
-        threeBTN.setOnClickListener(this)
-        fourBTN.setOnClickListener(this)
-        fiveBTN.setOnClickListener(this)
-        sixBTN.setOnClickListener(this)
-        sevenBTN.setOnClickListener(this)
-        eightBTN.setOnClickListener(this)
-        nineBTN.setOnClickListener(this)
-        sumBTN.setOnClickListener(this)
-        divBTN.setOnClickListener(this)
-        multBTN.setOnClickListener(this)
-        difBTN.setOnClickListener(this)
-        resultBTN.setOnClickListener(this)
-        resetBTN.setOnClickListener(this)
-
     }
 
+    fun addOperandInInputTV(operand: String) {
+        val expression: String = inputTV.text.toString()
+        val lastElement: Char = expression[expression.length - 1]
+        val size = expression.length
+        var searchOperand: Char = 'a'
 
-
-
-    override fun onClick(v: View) {
-        if (inputTV.text.isEmpty()) {
-            return
-        }
-        fun addValueInInputTV(str: String) {
-            if (inputTV.text.toString() == "O") {
-                inputTV.text = str
-            } else {
-                inputTV.text = (buildString {
-                    append(inputTV.text.toString())
-                    append(str)
-                })
-            }
-
-        }
-
-        fun addOperandInInputTV(operand: String) {
-            val expression: String = inputTV.text.toString()
-            val lastElement: Char = expression[expression.length - 1]
-            val size = expression.length
-            var searchOperand: Char = 'a'
-
-            for (i in expression) {
-                if (i == '+' || i == '-' || i == '*' || i == '/') {
-                    searchOperand = i
-                }
-            }
-
-            if (expression == "O") { //если не введён ни один символ, то операнд не вводится
-                inputTV.text = "O"
-            }
-
-            else if (lastElement == '+' || lastElement == '-' ||
-                lastElement == '*' || lastElement == '/') { //если последний элемент является операндом, то заменяем его на новый операнд
-                var expressionNew: String = ""
-                for (i in 0 .. expression.length - 2) {
-                    expressionNew += expression[i]
-                }
-                inputTV.text = expressionNew + operand
-            }
-
-            else if (searchOperand != 'a') { //если операнд уже есть в выражении то второй не водится
-                inputTV.text = expression
-            }
-
-            else { //во всех остальных случаях операнд добавляется к выражению
-                inputTV.text = expression + operand
+        for (i in expression) {
+            if (i == '+' || i == '-' || i == '*' || i == '/') {
+                searchOperand = i
             }
         }
 
-        fun result(expression: String) {
-            var prevValue: String = ""
-            var nextValue: String = ""
-            val size = expression.length
-            var operand: String = ""
-            val lastElement: Char = expression[expression.length - 1]
-
-            if (lastElement == '+' ||
-                lastElement == '-' ||
-                lastElement == '*' ||
-                lastElement == '/'
-            ) {
-                resultTV.text = "Не хватает данных"
-            }
-
-            else if (expression.length > 2) {
-                for (i in expression.indices) {
-                    if (expression[i] == '+' ||
-                        expression[i] == '-' ||
-                        expression[i] == '/' ||
-                        expression[i] == '*'
-                    ) {
-                        operand = expression[i].toString()
-                        for (i in 0..i - 1) {
-                            prevValue += expression[i]
-                        }
-                        for (i in i + 1..size - 1) {
-                            nextValue += expression[i]
-                        }
-                    }
-                }
-
-            }
-
-            if (prevValue != "" && nextValue != "" && operand != "") {
-                when (operand) {
-                    "+" -> {
-                        val res = prevValue.toDouble() + nextValue.toDouble()
-                        resultTV.text = res.toString()
-                    }
-
-                    "-" -> {
-                        val res = prevValue.toDouble() - nextValue.toDouble()
-                        resultTV.text = res.toString()
-                    }
-
-                    "*" -> {
-                        val res = prevValue.toDouble() * nextValue.toDouble()
-                        resultTV.text = res.toString()
-                    }
-
-                    "/" -> {
-                        if (prevValue != "0" && nextValue != "0") {
-                            val res = prevValue.toDouble() / nextValue.toDouble()
-                            resultTV.text = res.toString()
-                        } else {
-                            resultTV.text = "Деление на ноль недопустимо!"
-                            inputTV.text = "O"
-                        }
-                    }
-                }
-            } else {
-                resultTV.text = "Не хватает данных"
-            }
-
+        if (expression == "O") { //если не введён ни один символ, то операнд не вводится
+            inputTV.text = "O"
         }
 
-        when (v.id) {
-            R.id.zeroBTN -> addValueInInputTV("0")
-            R.id.oneBTN -> addValueInInputTV("1")
-            R.id.twoBTN -> addValueInInputTV("2")
-            R.id.threeBTN -> addValueInInputTV("3")
-            R.id.fourBTN -> addValueInInputTV("4")
-            R.id.fiveBTN -> addValueInInputTV("5")
-            R.id.sixBTN -> addValueInInputTV("6")
-            R.id.sevenBTN -> addValueInInputTV("7")
-            R.id.eightBTN -> addValueInInputTV("8")
-            R.id.nineBTN -> addValueInInputTV("9")
-            R.id.sumBTN -> addOperandInInputTV("+")
-            R.id.difBTN -> addOperandInInputTV("-")
-            R.id.multBTN -> addOperandInInputTV("*")
-            R.id.divBTN -> addOperandInInputTV("/")
-            R.id.resultBTN -> result(inputTV.text.toString())
-            R.id.resetBTN -> {
-                inputTV.text = "O"
-                resultTV.text = "Результат"
+        else if (lastElement == '+' || lastElement == '-' ||
+            lastElement == '*' || lastElement == '/') { //если последний элемент является операндом, то заменяем его на новый операнд
+            var expressionNew: String = ""
+            for (i in 0 .. expression.length - 2) {
+                expressionNew += expression[i]
             }
+            inputTV.text = expressionNew + operand
+        }
 
-            else -> {
-                inputTV.text = "O"
-                resultTV.text = "Результат"
-            }
+        else if (searchOperand != 'a') { //если операнд уже есть в выражении то второй не водится
+            inputTV.text = expression
+        }
+
+        else { //во всех остальных случаях операнд добавляется к выражению
+            inputTV.text = expression + operand
         }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main,menu)
